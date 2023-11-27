2e7848ddbfd11a5f684e3725600ea186b42778d3
GORDOYAMASSAHICK <117641677+GORDOYAMASSAHICK@users.noreply.github.com>
2023-11-27 10:09:50 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+function tocaSomPom(seletorAudio.){
+    constelemento=document.queryselector(idelementoAudio){constelemento=document.querySelector(idElementoAudio)M(elemento&&elemento.localName===audio){elemento.play(), else{console.log('elementonãoencontrado')}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+M(evento.code=== 'enter' or evento.code==='space'){tecla.classlist.add('ativa')
